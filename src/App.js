d283c3e2941b5f6f4393344a455b0d31030f8bae
Meri2233 <sharmamerika2@gmail.com>
2022-07-22 15:06:44 +0545

    replaced useState with useReducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import Holdings from "./components/Holdings";
 import Transactions from "./components/Tansactions";
 
+const ACTIONS={
+
+}
+ 
+function reducer(){
+
+}
 
 function App() {
-  let [money,setMoney] = useState("100")
-  let [value,setValue] = useState("0.00")
+  let [state,dispatch]= useReducer(reducer,{money:"100",value:"0.00"})
 
   return (
     <div className="App">
       <h2>Earn some virtual money 💰</h2>
       <p className="font">To buy virtual load</p>
-      <p className="wallet">🏦 Wallet: ${money}</p>
-      <p className="portfolio">Portfolio Value:${value}</p>
+      <p className="wallet">🏦 Wallet: ${state.money}</p>
+      <p className="portfolio">Portfolio Value:${state.value}</p>
       <div className="records">
         <Holdings/>
         <Transactions/>
